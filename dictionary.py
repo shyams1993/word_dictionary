47d6b8e05bdb1b30d9e033c578a676f1ccccc33d
Shyam Salil <shyamsalil1993@gmail.com>
2020-12-31 16:34:44 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif word.title() in data:
+        return data[word.title()]
+    elif len(get_close_matches(word , data.keys())) > 0 :
+        print("did you mean %s instead? " %get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes or n for no. ")
+        if decide == "y":
+            return data[get_close_matches(word , data.keys())[0]]
+        elif decide == "n":
+            return("Wrong word. ")
+        else:
+            return("You have entered wrong input please enter just y or n")
+    else:
+        print("Wrong word. ")
+
+
+word = input("Enter the word to search: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
